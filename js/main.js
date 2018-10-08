fc48f93193a0d9ece0d6718ae08e8e5a06008ef5
Sunday Brian <brians931@gmail.com>
2018-10-09 01:30:21 +0300

    fix the broken omdb api link

--- js/main.js
+++ js/main.js
@@ -12,7 +12,18 @@ $(document).ready(() => {
 
   function getMovies(searchText){
     console.log(searchText);
+    axios.get('http://www.omdbapi.com/?apikey=c729fcf0&s=' +searchText)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+
   }
+
+  $('#movies').html(response.data.Title);
 
 
 });
