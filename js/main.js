c69798d132ffa3c675df1bf44d3f3d4d1b4808f9
Sunday Brian <brians931@gmail.com>
2018-10-09 16:03:36 +0300

    Add functionalities to the getMovie function to append data to tthe movie.html page

--- js/main.js
+++ js/main.js
@@ -55,6 +55,51 @@ function getMovie() {
   axios.get('http://www.omdbapi.com/?apikey=c729fcf0&i=' + movieId)
     .then(function(response) {
       console.log(response);
+      let movie = response.data;
+      let output = `
+
+      <div class ="row">
+      <div class ="col-md-4">
+      <img src="${movie.Poster}" class="thumbnail">
+
+      </div>
+
+      <div class ="col-md-8">
+      <h2>${movie.Title}</h2>
+
+      <ul class="list-group">
+      <li class="list-group-item">Genre:${movie.Genre}</li>
+      <li class="list-group-item">Released:${movie.Released}</li>
+      <li class="list-group-item">Rated:${movie.Rated}</li>
+      <li class="list-group-item">Rating:${movie.imdbRating}</li>
+      <li class="list-group-item">Runtime:${movie.Runtime}</li>
+      <li class="list-group-item">Director:${movie.Director}</li>
+      <li class="list-group-item">Actors:${movie.Actors}</li>
+      <li class="list-group-item">Year:${movie.Year}</li>
+
+      </ul>
+
+      </div>
+      </div>
+
+      <div class="row">
+      <div class="well">
+      <h3>Plot</h3>
+      ${movie.Plot}
+
+      <hr>
+
+      </div>
+
+      </div>
+      <a href="http://imdb.com/title/${movie.imdbID}" target ="_blank" class="btn btn-primary">View IMDB</a>
+      <a href="index.html" class="btn ">Go Back</>
+
+      `;
+
+      $('#movie').html(output);
+
+
     })
     .catch(function(error) {
       console.log(error);
